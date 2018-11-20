0f785d4b4ff3baf6b2a053b2b0ad309e4147a948
Mahantesh Hiremath <uxg.mahantesh@gmail.com>
2018-11-20 15:44:25 +0530

    new comment added

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ ReactDOM.render(<PatientInformationForm />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//akshat soni
